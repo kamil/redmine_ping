7faf1e4fa816d9f004f566693815b35980823227
Kamil Załęski <kamil.zaleski@gmail.com>
2014-03-31 15:59:59 +0200

    move WindowController to new file

--- init.rb
+++ init.rb
@@ -4,6 +4,8 @@ require_dependency 'redmine_ping/hooks'
 Redmine::Plugin.register :redmine_ping do
   name 'Redmine Ping'
   author 'Kamil Załęski'
+  author_url 'https://kamilzaleski.com/'
   description ''
   version '0.0.1'
+  url 'https://github.com/kamil/redmine_ping'
 end
